03934755d0017228ee3e5b2178ab708e039c2e1c
Phillip Kim <phillipkim156@gmail.com>
2025-09-21 22:00:15 +1200

    Added gestures for stickers

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,10 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function RootLayout() {
   return (
-    <>
+    <GestureHandlerRootView style={{ flex: 1 }}>
       <StatusBar style="light" />
       <Stack>
         <Stack.Screen
@@ -14,6 +15,6 @@ export default function RootLayout() {
         />
         <Stack.Screen name="+not-found" options={{ headerShown: false }} />
       </Stack>
-    </>
+    </GestureHandlerRootView>
   );
 }
